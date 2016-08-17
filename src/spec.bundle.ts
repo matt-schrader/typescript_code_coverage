ddffebb6b92b7de27346d68c3ccebe67cf101de5
Matthew Schrader <matthew.schrader@jda.com>
2016-08-17 14:51:09 -0500

    Require each file manually instead of looping over them because code coverage isn't working witht he way the loop includes the files

--- src/spec.bundle.ts
+++ src/spec.bundle.ts
@@ -1,5 +1,7 @@
-requireAll((<any>require).context("./", true, /spec.ts$/));
+// requireAll((<any>require).context("./", true, /spec.ts$/));
+//
+// function requireAll(r: any): any {
+//     r.keys().forEach(r);
+// }
 
-function requireAll(r: any): any {
-    r.keys().forEach(r);
-}
+require('./greeter/greeter.spec.ts')
